67460168a788d2d9fd9f92e0472db18f8b91f9f1
= <=>
2024-06-28 00:34:24 -0400

    funcional chat

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,19 @@
+class RoomsController < ApplicationController
+  def index
+    @room = Room.new
+    @rooms = Room.public_rooms
+    @users = User.all_except(current_user)
+  end
+  def create
+    @room = Room.create(name: params["room"]["name"])
+  end
+  def show
+    @single_room = Room.find(params[:id])
+    @rooms = Room.public_rooms
+    @users = User.all_except(current_user)
+    @room = Room.new
+    @message = Message.new
+    @messages = @single_room.messages
+    render "index"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'rooms/index'
+  get 'home/index'
+  root 'home#index'
+  resources :rooms
+  resources :users
+  resources :rooms do
+    resources :messages
+  end
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  #problema al cerrar la sesion con devise 
+  devise_scope :user do 
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end 
+end
