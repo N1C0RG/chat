a35dd644b7dde7725844d00ea8da614c6a65163a
= <=>
2024-06-28 20:53:22 -0400

    un poco visual

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'rooms/index'
-  get 'home/index'
+  get 'home/index', as: 'home'
   root 'home#index'
   resources :rooms
   resources :users
